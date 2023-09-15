133565de07e78e1b6918ee43714e6a77704ec114
tree478 <ssshel25@students.shrewsbury.k12.ma.us>
2023-09-14 21:24:30 -0400

    Initial Commit

---
+++ Diamond.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import java.awt.event.*;
+import kareltherobot.*;
+
+public class Diamond implements Directions
+{
+
+	void turnRight(Robot bot){
+		bot.turnLeft();
+		bot.turnLeft();
+		bot.turnLeft();
+	}
+  public static void main(String[] args) 
+	{
+
+		//My robot's name is bobbie
+    	Robot bobbie = new Robot(20,1,East,400);
+		
+		World.setVisible(true);
+		World.setDelay(0);
+		World.setSize(20, 20);
+
+		for(int i=0; i<20; i++){
+			for(int s=0; s<i; s++){
+				bobbie.putBeeper();
+				bobbie.move();
+			}
+			//bobbie.turnRight(bobbie);
+			bobbie.turnLeft();
+			bobbie.turnLeft();
+			bobbie.turnLeft();
+			bobbie.move();
+			//bobbie.turnRight(bobbie);
+			bobbie.turnLeft();
+			bobbie.turnLeft();
+			bobbie.turnLeft();
+			for(int b=0; b<i; b++){
+				bobbie.move();
+			}
+			bobbie.turnLeft();
+			bobbie.turnLeft();
+
+		}
+		
+	}
+
+	}
