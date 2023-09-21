1c660b2d7a5b2b2e32a832df8e5439fbc58890a1
tree478 <ssshel25@students.shrewsbury.k12.ma.us>
2023-09-21 06:22:32 -0400

    New Method

--- TagTeam.java
+++ TagTeam.java
@@ -26,18 +26,22 @@ public class TagTeam implements Directions {
             bob.move();
             bob.turnLeft();
         }
+
+        System.out.println("Finished climbStairs()");
     }
 
     public static void makePeak(Robot bob){
         turnRight(bob);
         bob.putBeeper();
+        turnRight(bob);
+        
         bob.turnLeft();
         for(int i=0; i<3; i++){
             bob.move();
-            bob.turnLeft();
+            turnRight(bob);
             bob.move();
             bob.putBeeper();
-            turnRight(bob);
+            bob.turnLeft();
 
         }
 
@@ -47,35 +51,68 @@ public class TagTeam implements Directions {
         bob.move();
         bob.putBeeper();
 
-        bob.turnLeft();
         for(int i=0; i<3; i++){
             bob.move();
-            turnRight(bob);
+            bob.turnLeft();
             bob.move();
             bob.putBeeper();
-            bob.turnLeft();
+            turnRight(bob);
 
         }
 
     }
+
+    public static void thirdMethod(Robot bob){
+        for(int i=0; i<5; i++){
+            bob.move();
+            bob.putBeeper();
+        }
+        
+        turnRight(bob);
+        bob.move();
+        bob.putBeeper();
+        bob.move();
+        bob.putBeeper();
+
+        bob.move();
+        turnRight(bob);
+        bob.move();
+        bob.putBeeper();
+        bob.turnLeft();
+        bob.move();
+        bob.turnLeft();
+        bob.move();
+        bob.putBeeper();
+
+        bob.turnLeft();
+        bob.turnLeft();
+
+        bob.move();
+        bob.putBeeper();
+        bob.move();
+        bob.putBeeper();
+    }
+
     public static void main(String[] args){
         Robot bob = new Robot(1, 1, North, 400);
 
         World.setVisible(true);
 		World.setDelay(0);
-		World.setSize(10, 20);
+		World.setSize(15, 20);
 
         climbStairs(bob);
 
         World.setDelay(10);
 
-        bob.turnLeft();
-        bob.turnLeft();
-        for(int i=0; i<8; i++){
-            bob.move();
-        }
-        bob.turnLeft();
-        bob.turnLeft();
+        // bob.turnLeft();
+        // bob.turnLeft();
+        // for(int i=0; i<8; i++){
+        //     bob.move();
+        // }
+        // bob.turnLeft();
+        // bob.turnLeft();
         makePeak(bob);
+
+        thirdMethod(bob);
     }
 }
